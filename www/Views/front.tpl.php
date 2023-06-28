64f8cb138db4c3f4efd5fd7c1134042919f5ecf7
Karnaa07 <quentingrd0706@gmail.com>
2023-06-28 12:15:06 +0200

    modif

--- www/Views/front.tpl.php
+++ www/Views/front.tpl.php
@@ -5,18 +5,13 @@
     <title><?= $pageTitle ?></title>
     <meta name="description" content="ceci est un super site">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp">
-
-
-
     <link rel="stylesheet" type="text/css" href="../src/style.css">
+    <script src= "../src/script.js"></script>
 </head>
 <body>
     <h1>Template Front</h1>
 
     <!-- inclure la vue -->
     <?php include $this->view;?>
-
-
-    <script src= "../src/script.js"></script>
 </body>
 </html>
