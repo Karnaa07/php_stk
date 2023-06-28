c89fbcf9b91dc41603a788d5fb6a3905dc0d5e87
Karnaa07 <quentingrd0706@gmail.com>
2023-06-28 10:50:13 +0200

    add template for all page back

--- www/Views/Main/home.view.php
+++ www/Views/Main/home.view.php
@@ -1,11 +1,2 @@
 <h2>Welcome <?= $pseudo; ?> </h2>
 <h3><?= $age?></h3>
- 
-<!-- Bouton de déconnexion -->
-<a href="/logout">Logout</a>
-
-<!-- Bouton de contact -->
-<a href="/contact">Contact</a>
-
-<!-- Bouton de nous -->
-<a href="/about-us">about-us</a>
