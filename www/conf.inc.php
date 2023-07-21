8f6090fbbba008c7ce9319de7620305f24f9fb13
Karnaa07 <quentingrd0706@gmail.com>
2023-07-21 19:12:26 +0200

    supp de git conf.inc.php

--- www/conf.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config['host'] = 'database';
-$config['dbname'] = 'esgi';
-$config['port'] = '5432';
-$config['user'] = 'esgi';
-$config['password'] = 'Test1234';
-$config['dbprefix'] = 'esgi';
